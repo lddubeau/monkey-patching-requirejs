6cdedb2cb00ee60490ecad7c7480beb1f34a6395
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-11-01 08:28:10 -0400

    Made _dynamic's implementation be detemined at module loading.

--- moduleB.js
+++ moduleB.js
@@ -4,6 +4,22 @@ define(["module"], function (module) {
     var debug = module.config().debug;
     var exports = {};
 
+    /**
+     * @function
+     * @param {String} name Name of the fuction to export
+     * @param {Function} f Function to export.
+     * @returns {Function} A wrapper for <code>f</code>, or <code>f</code>.
+     */
+    var _dynamic = (debug ?
+        function (name, f) {
+            exports[name] = f;
+            return function () {
+                // This call allows for future changes to arguments passed..
+                return exports[name].apply(this, arguments);
+            };
+        } :
+        _dynamic = function (name, f) { return f; });
+
     var foo = function () {
         return bar(1, 2, 3);
     };
@@ -11,23 +27,6 @@ define(["module"], function (module) {
     var bar = _dynamic("bar", function (a, b, c) {
         return "original: called with " + a + " " + b + " " + c;
     });
-
-    /**
-     * @param {String} name Name of the fuction to export
-     * @param {Function} f Function to export.
-     * @returns {Function} A local wrapper for <code>f</code>.
-     */
-    function _dynamic(name, f) {
-        if (debug) {
-            exports[name] = f;
-            return function () {
-                // This call allows for future changes to arguments passed..
-                return exports[name].apply(this, arguments);
-            };
-        }
-        else
-            return f;
-    }
 
     exports.foo = foo;
 
